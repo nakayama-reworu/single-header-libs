6585f587bee0d48fbe3ad049ec77172b5a292be9
reworu <nakayama.reworu@gmail.com>
2026-05-11 01:19:28 +0700

    Rename assert_that to assert_always, exit on malloc/realloc failure

--- collections/vector/vector.c
+++ collections/vector/vector.c
@@ -17,7 +17,7 @@ fprintf(                                        \
 #ifdef assert
 #undef assert
 #endif
-#define assert_that(expr)                           \
+#define assert_always(expr)                         \
 do {                                                \
     if (expr) { break; }                            \
     fprintf(                                        \
@@ -26,7 +26,9 @@ do {                                                \
         __FILE_NAME__, __LINE__, __FUNCTION__       \
     );                                              \
     exit(EXIT_FAILURE);                             \
-} while (0)\
+} while (0)
+
+#define on_malloc_error() exit(EXIT_FAILURE)
 
 typedef struct {
     size_t size;
@@ -53,8 +55,9 @@ void *vector_new(size_t element_size, size_t elements_count) {
     Header *header = calloc(1, total_size);
     if (NULL == header) {
         print_error(calloc);
-        return NULL;
+        on_malloc_error();
     }
+
     header->size = elements_count;
     header->capacity = capacity;
     header->element_size = element_size;
@@ -63,37 +66,37 @@ void *vector_new(size_t element_size, size_t elements_count) {
 }
 
 void vector_free_(void *vector) {
-    assert_that(NULL != vector);
+    assert_always(NULL != vector);
     free(vector_header(vector));
 }
 
 #define vector_at(header, index) ((header)->data + (header)->element_size * (index))
 
 const void *vector_end_(const void *vector) {
-    assert_that(NULL != vector);
+    assert_always(NULL != vector);
 
     const Header *header = vector_header_const(vector);
     return vector_at(header, header->size);
 }
 
 size_t vector_size(const void *vector) {
-    assert_that(NULL != vector);
+    assert_always(NULL != vector);
     return vector_header_const(vector)->size;
 }
 
 bool vector_empty(const void *vector) {
-    assert_that(NULL != vector);
+    assert_always(NULL != vector);
     return 0 == vector_size(vector);
 }
 
 static void vector_set_from(Header *header, size_t index, const void *value) {
-    assert_that(index < header->size);
+    assert_always(index < header->size);
 
     memcpy(vector_at(header, index), value, header->element_size);
 }
 
 static void vector_get_to(const Header *header, size_t index, void *dst) {
-    assert_that(index < header->size);
+    assert_always(index < header->size);
 
     memcpy(dst, vector_at(header, index), header->element_size);
 }
@@ -103,7 +106,7 @@ static Header *vector_realloc(Header *header, size_t new_capacity) {
     Header *new_header = realloc(header, new_total_size);
     if (NULL == new_header) {
         print_error(realloc);
-        return NULL;
+        on_malloc_error();
     }
 
     new_header->capacity = new_capacity;
@@ -112,13 +115,13 @@ static Header *vector_realloc(Header *header, size_t new_capacity) {
 
 static size_t increased_capacity(size_t capacity) {
     const size_t new_capacity = capacity * 3 / 2 + 1;
-    assert_that(new_capacity > capacity);
+    assert_always(new_capacity > capacity);
     return new_capacity;
 }
 
 void *vector_with_appended_from(void *vector, const void *value) {
-    assert_that(NULL != vector);
-    assert_that(NULL != value);
+    assert_always(NULL != vector);
+    assert_always(NULL != value);
 
     Header *header = vector_header(vector);
     if (header->size == header->capacity) {
@@ -135,8 +138,8 @@ void *vector_with_appended_from(void *vector, const void *value) {
 }
 
 void *vector_with_range_appended_from(void *vector, const void *src, size_t count) {
-    assert_that(NULL != vector);
-    assert_that(NULL != src);
+    assert_always(NULL != vector);
+    assert_always(NULL != src);
 
     Header *header = vector_header(vector);
     const size_t total_size = header->size + count;
@@ -161,8 +164,8 @@ void *vector_with_range_appended_from(void *vector, const void *src, size_t coun
 }
 
 bool vector_pop_to(void *vector, void *dst) {
-    assert_that(NULL != vector);
-    assert_that(NULL != dst);
+    assert_always(NULL != vector);
+    assert_always(NULL != dst);
 
     Header *header = vector_header(vector);
     if (0 == header->size) {
