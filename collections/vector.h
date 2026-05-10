f7b999ef99fb0dab443dc59d562d739c5b279aba
reworu <nakayama.reworu@gmail.com>
2026-05-11 01:19:32 +0700

    Make Vector_* macros store vector argument in a variable to avoid possible re-evaluation

--- collections/vector.h
+++ collections/vector.h
@@ -23,58 +23,62 @@ do {                                        \
     *(VecPtr) = (typeof(*(VecPtr))) { 0 };  \
 } while (0)
 
-#define Vector_PushBack(VecPtr, Val)                            \
-do {                                                            \
-    if ((VecPtr)->Size >= (VecPtr)->Capacity) {                 \
-        (VecPtr)->Capacity =                                    \
-            3 * (VecPtr)->Capacity / 2 + 1;                     \
-        __auto_type _items = CallChecked(realloc, (             \
-            (VecPtr)->Items,                                    \
-            (VecPtr)->Capacity * sizeof((VecPtr)->Items[0])     \
-        ));                                                     \
-        (VecPtr)->Items = _items;                               \
-    }                                                           \
-    (VecPtr)->Items[(VecPtr)->Size++] = (Val);                  \
+#define Vector_PushBack(VecPtr, Val)                                        \
+do {                                                                        \
+    __auto_type _vec_push_back = (VecPtr);                                  \
+    if (_vec_push_back->Size >= _vec_push_back->Capacity) {                 \
+        _vec_push_back->Capacity =                                          \
+            3 * _vec_push_back->Capacity / 2 + 1;                           \
+        __auto_type _items = CallChecked(realloc, (                         \
+            _vec_push_back->Items,                                          \
+            _vec_push_back->Capacity * sizeof(_vec_push_back->Items[0])     \
+        ));                                                                 \
+        _vec_push_back->Items = _items;                                     \
+    }                                                                       \
+    _vec_push_back->Items[_vec_push_back->Size++] = (Val);                  \
 } while (0)
 
-#define Vector_TryPopBack(VecPtr, ValuePtr)         \
-({                                                  \
-    bool _ok = false;                               \
-    if ((VecPtr)->Size > 0) {                       \
-        *(ValuePtr) =                               \
-            (VecPtr)->Items[--(VecPtr)->Size];      \
-        _ok = true;                                 \
-    }                                               \
-    _ok;                                            \
+#define Vector_TryPopBack(VecPtr, ValuePtr)                         \
+({                                                                  \
+    __auto_type _vec_try_pop_back = (VecPtr);                       \
+    bool _ok = false;                                               \
+    if (_vec_try_pop_back->Size > 0) {                              \
+        *(ValuePtr) =                                               \
+            _vec_try_pop_back->Items[--_vec_try_pop_back->Size];    \
+        _ok = true;                                                 \
+    }                                                               \
+    _ok;                                                            \
 })
 
-#define Vector_At(Vec, Index)               \
-({                                          \
-    long long _idx = (Index);               \
-    long long _sz = (Vec).Size;             \
-    typeof((Vec).Items[0]) *_value = NULL;  \
-    if (0 <= _idx && _idx < _sz) {          \
-        _value = (Vec).Items + _idx;        \
-    }                                       \
-    if (-_sz <= _idx && _idx < 0) {         \
-        _value = (Vec).Items + _sz + _idx;  \
-    }                                       \
-    _value;                                 \
+#define Vector_At(Vec, Index)                   \
+({                                              \
+    __auto_type _vec_at = (Vec);                \
+    long long _idx = (Index);                   \
+    long long _sz = _vec_at.Size;               \
+    typeof(_vec_at.Items[0]) *_value = NULL;    \
+    if (0 <= _idx && _idx < _sz) {              \
+        _value = _vec_at.Items + _idx;          \
+    }                                           \
+    if (-_sz <= _idx && _idx < 0) {             \
+        _value = _vec_at.Items + _sz + _idx;    \
+    }                                           \
+    _value;                                     \
 })
 
-#define Vector_ForEach(ValuePtr, Vec)                       \
-size_t VECTOR_CONCAT(_it_, __LINE__) = 0;                   \
-for (                                                       \
-    typeof(*((Vec).Items)) *ValuePtr =                      \
-        VECTOR_CONCAT(_it_, __LINE__) >= (Vec).Size         \
-            ? NULL                                          \
-            : &(Vec).Items[VECTOR_CONCAT(_it_, __LINE__)];  \
-    VECTOR_CONCAT(_it_, __LINE__) < (Vec).Size;             \
-    VECTOR_CONCAT(_it_, __LINE__)++,                        \
-    ValuePtr =                                              \
-        VECTOR_CONCAT(_it_, __LINE__) >= (Vec).Size         \
-            ? NULL                                          \
-            : &(Vec).Items[VECTOR_CONCAT(_it_, __LINE__)]   \
+#define Vector_ForEach(ValuePtr, Vec)                                                   \
+size_t VECTOR_CONCAT(_it_, __LINE__) = 0;                                               \
+__auto_type VECTOR_CONCAT(_vec_for_, __LINE__) = (Vec);                                 \
+for (                                                                                   \
+    typeof(*(VECTOR_CONCAT(_vec_for_, __LINE__).Items)) *ValuePtr =                     \
+        VECTOR_CONCAT(_it_, __LINE__) >= VECTOR_CONCAT(_vec_for_, __LINE__).Size        \
+            ? NULL                                                                      \
+            : &VECTOR_CONCAT(_vec_for_, __LINE__).Items[VECTOR_CONCAT(_it_, __LINE__)]; \
+    VECTOR_CONCAT(_it_, __LINE__) < VECTOR_CONCAT(_vec_for_, __LINE__).Size;            \
+    VECTOR_CONCAT(_it_, __LINE__)++,                                                    \
+    ValuePtr =                                                                          \
+        VECTOR_CONCAT(_it_, __LINE__) >= VECTOR_CONCAT(_vec_for_, __LINE__).Size        \
+            ? NULL                                                                      \
+            : &VECTOR_CONCAT(_vec_for_, __LINE__).Items[VECTOR_CONCAT(_it_, __LINE__)]  \
 )
 
 #define Vector_Empty(Vec)       (0 == (Vec).Size)
