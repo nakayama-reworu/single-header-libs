2e33cb5aa703aa9fdbcb74ad3d9344d6c932ff0b
reworu <nakayama.reworu@gmail.com>
2026-05-11 01:19:34 +0700

    Rename Vector_Of type constructor to Vector, implement Vector_Empty, Vector_FromPtr, Vector_FromArray, Vector_From and Vector_Of object constructors, add tests

--- collections/vector.h
+++ collections/vector.h
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define VECTOR__ArrayLength(Array)      (sizeof(Array) / sizeof(*(Array)))
+#define VECTOR__ToArrayLiteral(_0, ...) ((typeof(_0)[]) {_0, ##__VA_ARGS__})
+
 #define VECTOR__CallChecked(Callee, ArgsList)   \
 ({                                          \
     errno = 0;                              \
@@ -21,15 +24,35 @@
     _r;                                     \
 })
 
-#define VECTOR__Concat_(A, B)   A ## B
-#define VECTOR__Concat(A, B)    VECTOR__Concat_(A, B)
-
-#define Vector_Of(TValue) \
-struct {                  \
-    size_t Size;          \
-    size_t Capacity;      \
-    TValue *Items;        \
+#define Vector(TValue)  \
+struct {                \
+    size_t Size;        \
+    size_t Capacity;    \
+    TValue *Items;      \
 }
+
+#define Vector_Empty(VectorType) ((VectorType) {0})
+
+#define Vector_FromPtr(VectorType, Ptr, Count)              \
+({                                                          \
+    VectorType _vec_fromArray = Vector_Empty(VectorType);   \
+    typeof(*_vec_fromArray.Items) *_arr = (Ptr);            \
+    size_t const _count = (Count);                          \
+    for (size_t _i = 0; _i < _count; _i++) {                \
+        Vector_PushBack(&_vec_fromArray, _arr[_i]);         \
+    }                                                       \
+    _vec_fromArray;                                         \
+})
+
+#define Vector_FromArray(VectorType, Array) Vector_FromPtr(VectorType, (Array), VECTOR__ArrayLength(Array))
+
+#define Vector_From(VectorType, Src)                                \
+({                                                                  \
+    __auto_type _src_from = (Src);                                  \
+    Vector_FromPtr(VectorType, _src_from.Items, _src_from.Size);    \
+})
+
+#define Vector_Of(VectorType, ...) Vector_FromArray(VectorType, VECTOR__ToArrayLiteral(__VA_ARGS__))
 
 #define Vector_Free(VecPtr)                 \
 do {                                        \
@@ -64,91 +87,8 @@ do {                                                                        \
     _ok;                                                            \
 })
 
-#define Vector_At(Vec, Index)                   \
-({                                              \
-    __auto_type _vec_at = (Vec);                \
-    long long _idx = (Index);                   \
-    long long _sz = _vec_at.Size;               \
-    typeof(_vec_at.Items[0]) *_value = NULL;    \
-    if (0 <= _idx && _idx < _sz) {              \
-        _value = _vec_at.Items + _idx;          \
-    }                                           \
-    if (-_sz <= _idx && _idx < 0) {             \
-        _value = _vec_at.Items + _sz + _idx;    \
-    }                                           \
-    _value;                                     \
-})
+#define Vector_IsEmpty(Vec) (0 == (Vec).Size)
 
-#define Vector_ForEach(ValuePtr, Vec)                                                       \
-size_t VECTOR__Concat(_it_, __LINE__) = 0;                                                  \
-__auto_type VECTOR__Concat(_vec_for_, __LINE__) = (Vec);                                    \
-for (                                                                                       \
-    typeof(*(VECTOR__Concat(_vec_for_, __LINE__).Items)) *ValuePtr =                        \
-        VECTOR__Concat(_it_, __LINE__) >= VECTOR__Concat(_vec_for_, __LINE__).Size          \
-            ? NULL                                                                          \
-            : &VECTOR__Concat(_vec_for_, __LINE__).Items[VECTOR__Concat(_it_, __LINE__)];   \
-    VECTOR__Concat(_it_, __LINE__) < VECTOR__Concat(_vec_for_, __LINE__).Size;              \
-    VECTOR__Concat(_it_, __LINE__)++,                                                       \
-    ValuePtr =                                                                              \
-        VECTOR__Concat(_it_, __LINE__) >= VECTOR__Concat(_vec_for_, __LINE__).Size          \
-            ? NULL                                                                          \
-            : &VECTOR__Concat(_vec_for_, __LINE__).Items[VECTOR__Concat(_it_, __LINE__)]    \
-)
-
-#define Vector_Empty(Vec)       (0 == (Vec).Size)
-
-#define Vector_Clear(VecPtr)    do { (VecPtr)->Size = 0; } while (0)
-
-#define Vector_Any(Vec, Value, PredicateExpr)   \
-({                                              \
-    bool _matches = false;                      \
-    Vector_ForEach(p, (Vec)) {                  \
-        __auto_type Value = *p;                 \
-        if ((PredicateExpr)) {                  \
-            _matches = true;                    \
-            break;                              \
-        }                                       \
-    }                                           \
-    _matches;                                   \
-})
-
-#define VectorSlice_Of(TValue)    \
-struct {                    \
-    size_t Size;            \
-    TValue *Items;          \
-}
-
-#define Vector_Slice(TSlice, Vec, Start, End)           \
-({                                                      \
-    __auto_type _vec_slice = (Vec);                     \
-    long long _sz_slice = _vec_slice.Size;              \
-    long long _start = (Start);                         \
-    if (_start < 0) _start = 0;                         \
-    if (_start > _sz_slice) _start = _sz_slice;         \
-    long long _end = (End);                             \
-    if (_end < 0) _end = 0;                             \
-    if (_end > _sz_slice) _end = _sz_slice;             \
-    __auto_type _items = Vector_At(_vec_slice, _start); \
-    bool _ok = 0 <= _start                              \
-        && _start < _sz_slice                           \
-        && _end >= _start                               \
-        && _end <= _sz_slice;                           \
-    (TSlice) {                                          \
-        .Size=(_ok ? (_end-_start) : 0),                \
-        .Items=(_ok ? _items : NULL),                   \
-    };                                                  \
-})
-
-#define Vector_SliceFrom(TSlice, Vec, Start)                                \
-({                                                                          \
-    __auto_type _vec_slice_from = (Vec);                                    \
-    Vector_Slice(TSlice, _vec_slice_from, (Start), _vec_slice_from.Size);   \
-})
-
-#define Vector_SliceTo(TSlice, Vec, End)            \
-({                                                  \
-    __auto_type _vec_slice_to = (Vec);              \
-    Vector_Slice(TSlice, _vec_slice_to, 0, (End));  \
-})
+#define Vector_Clear(VecPtr) do { (VecPtr)->Size = 0; } while (0)
 
 #endif // VECTOR_H
--- collections/vector_test.c
+++ collections/vector_test.c
@@ -6,15 +6,76 @@
 
 #include "testing/testing.h"
 
-Testing_Fact(empty_vector_has_size_and_capacity_of_0) {
-    Vector_Of(int) sut = {0};
+typedef Vector(int) IntVector;
+
+Testing_Fact(Empty_returns_vector_with_Size_and_Capacity_set_to_0) {
+    IntVector sut = Vector_Empty(IntVector);
 
     Testing_Assert(0 == sut.Size, "size must be 0 for empty vector");
     Testing_Assert(0 == sut.Capacity, "capacity must be 0 for empty vector");
 }
 
+Testing_Fact(FromPtr_returns_vector_with_elements_from_given_address) {
+    int nums[] = {1, 2, 3, 4, 5};
+    size_t const numsSize = sizeof(nums) / sizeof(*nums);
+
+    IntVector sut = Vector_FromPtr(IntVector, nums, numsSize);
+
+    Testing_Assert(numsSize == sut.Size, "wrong Size");
+    Testing_Assert(0 == memcmp(nums, sut.Items, numsSize * sizeof(int)), "wrong contents");
+
+    Vector_Free(&sut);
+}
+
+Testing_Fact(FromArray_returns_vector_with_elements_from_given_automatic_array) {
+    int nums[] = {1, 2, 3, 4, 5};
+    size_t const numsSize = sizeof(nums) / sizeof(*nums);
+
+    IntVector sut = Vector_FromArray(IntVector, nums);
+
+    Testing_Assert(numsSize == sut.Size, "wrong Size");
+    Testing_Assert(0 == memcmp(nums, sut.Items, numsSize * sizeof(int)), "wrong contents");
+
+    Vector_Free(&sut);
+}
+
+Testing_Fact(FromArray_returns_vector_with_elements_from_given_array_literal) {
+    int nums[] = {1, 2, 3, 4, 5};
+    size_t const numsSize = sizeof(nums) / sizeof(*nums);
+
+    IntVector sut = Vector_FromArray(IntVector, ((int[]) {1, 2, 3, 4, 5}));
+
+    Testing_Assert(numsSize == sut.Size, "wrong Size");
+    Testing_Assert(0 == memcmp(nums, sut.Items, numsSize * sizeof(int)), "wrong contents");
+
+    Vector_Free(&sut);
+}
+
+Testing_Fact(From_returns_vector_with_elements_from_given_struct) {
+    IntVector src = Vector_FromArray(IntVector, ((int[]) {1, 2, 3, 4, 5}));
+
+    IntVector sut = Vector_From(IntVector, src);
+
+    Testing_Assert(src.Size == sut.Size, "wrong Size");
+    Testing_Assert(0 == memcmp(src.Items, sut.Items, src.Size * sizeof(int)), "wrong contents");
+
+    Vector_Free(&sut);
+}
+
+Testing_Fact(Of_returns_vector_with_given_elements) {
+    int nums[] = {1, 2, 3, 4, 5};
+    size_t const numsSize = sizeof(nums) / sizeof(*nums);
+
+    IntVector sut = Vector_Of(IntVector, 1, 2, 3, 4, 5);
+
+    Testing_Assert(numsSize == sut.Size, "wrong Size");
+    Testing_Assert(0 == memcmp(nums, sut.Items, numsSize * sizeof(int)), "wrong contents");
+
+    Vector_Free(&sut);
+}
+
 Testing_Fact(PushBack_appends_elements) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
     int const elements[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
     size_t const elementsCount = sizeof(elements) / sizeof(elements[0]);
@@ -35,7 +96,7 @@ Testing_Fact(PushBack_appends_elements) {
 }
 
 Testing_Fact(Free_sets_size_and_capacity_to_0) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
     Vector_PushBack(&sut, 1);
     Vector_PushBack(&sut, 2);
@@ -47,14 +108,14 @@ Testing_Fact(Free_sets_size_and_capacity_to_0) {
 }
 
 Testing_Fact(TryPopBack_returns_false_for_empty_vector) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
     int value;
     Testing_Assert(false == Vector_TryPopBack(&sut, &value), "expected TryPopBack to return false for empty vector");
 }
 
 Testing_Fact(TryPopBack_returns_elements_in_reverse_order) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
     int const elements[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
     size_t const elementsCount = sizeof(elements) / sizeof(elements[0]);
@@ -74,113 +135,28 @@ Testing_Fact(TryPopBack_returns_elements_in_reverse_order) {
     Vector_Free(&sut);
 }
 
-Testing_Fact(ForEach_never_executes_body_for_empty_list) {
-    Vector_Of(int) sut = {0};
-
-    int bodyExecuted = false;
-    Vector_ForEach(pValue, sut) {
-        (void) pValue;
-        bodyExecuted = true;
-    }
-
-    Testing_Assert(false == bodyExecuted, "body must not be executed for empty list");
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(ForEach_iterates_over_all_elements) {
-    Vector_Of(int) sut = {0};
-
-    int const elements[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    size_t const elementsCount = sizeof(elements) / sizeof(elements[0]);
-    for (size_t i = 0; i < elementsCount; i++) {
-        Vector_PushBack(&sut, elements[i]);
-    }
-
-    size_t j = 0;
-    Vector_ForEach(pValue, sut) {
-        Testing_Assert(elements[j] == *pValue, "expected value %zu to be %d but was %d", j, elements[j], *pValue);
-        j++;
-    }
-    Testing_Assert(
-            elementsCount == j,
-            "expected count of elements iterated over to be %zu but was %zu", elementsCount, j
-    );
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(At_returns_pointer_to_element_for_valid_non_negative_index) {
-    Vector_Of(int) sut = {0};
-
-    int const elements[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    size_t const elementsCount = sizeof(elements) / sizeof(elements[0]);
-    for (size_t i = 0; i < elementsCount; i++) {
-        Vector_PushBack(&sut, elements[i]);
-    }
-
-    for (size_t i = 0; i < sut.Size; i++) {
-        int const *valuePtr = Vector_At(sut, i);
-        Testing_Assert(&sut.Items[i] == valuePtr, "At returned wrong pointer");
-    }
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(At_returns_pointer_to_element_for_valid_negative_index) {
-    Vector_Of(int) sut = {0};
-
-    int const elements[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    size_t const elementsCount = sizeof(elements) / sizeof(elements[0]);
-    for (size_t i = 0; i < elementsCount; i++) {
-        Vector_PushBack(&sut, elements[i]);
-    }
-
-    for (int i = 1; i <= (int) sut.Size; i++) {
-        int const *valuePtr = Vector_At(sut, -i);
-        Testing_Assert(&sut.Items[sut.Size - i] == valuePtr, "At returned wrong pointer");
-    }
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(At_returns_NULL_for_invalid_indices) {
-    Vector_Of(int) sut = {0};
-
-    int const elements[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    size_t const elementsCount = sizeof(elements) / sizeof(elements[0]);
-    for (size_t i = 0; i < elementsCount; i++) {
-        Vector_PushBack(&sut, elements[i]);
-    }
-
-    Testing_Assert(NULL == Vector_At(sut, sut.Size), "expected At to return NULL");
-    Testing_Assert(NULL == Vector_At(sut, -(int) sut.Size - 1), "expected At to return NULL");
-
-    Vector_Free(&sut);
-}
-
 Testing_Fact(Empty_returns_true_for_empty_vector) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
-    Testing_Assert(Vector_Empty(sut), "expected Empty to return true");
+    Testing_Assert(Vector_IsEmpty(sut), "expected Empty to return true");
 
     Vector_Free(&sut);
 }
 
 Testing_Fact(Empty_returns_false_for_non_empty) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
     Vector_PushBack(&sut, 1);
     Vector_PushBack(&sut, 2);
     Vector_PushBack(&sut, 3);
 
-    Testing_Assert(false == Vector_Empty(sut), "expected Empty to return false");
+    Testing_Assert(false == Vector_IsEmpty(sut), "expected Empty to return false");
 
     Vector_Free(&sut);
 }
 
 Testing_Fact(Empty_returns_true_for_vector_with_all_elements_removed) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
     Vector_PushBack(&sut, 1);
     Vector_PushBack(&sut, 2);
@@ -189,13 +165,13 @@ Testing_Fact(Empty_returns_true_for_vector_with_all_elements_removed) {
     int value;
     while (Vector_TryPopBack(&sut, &value)) {}
 
-    Testing_Assert(Vector_Empty(sut), "expected Empty to return true");
+    Testing_Assert(Vector_IsEmpty(sut), "expected Empty to return true");
 
     Vector_Free(&sut);
 }
 
 Testing_Fact(Empty_returns_true_after_Clear) {
-    Vector_Of(int) sut = {0};
+    IntVector sut = Vector_Empty(IntVector);
 
     Vector_PushBack(&sut, 1);
     Vector_PushBack(&sut, 2);
@@ -203,216 +179,26 @@ Testing_Fact(Empty_returns_true_after_Clear) {
 
     Vector_Clear(&sut);
 
-    Testing_Assert(Vector_Empty(sut), "expected Empty to return true");
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Any_returns_false_if_no_elements_match) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-
-    Testing_Assert(false == Vector_Any(sut, value, 42 == value), "expected Any to return false");
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Any_returns_true_if_an_elements_matches) {
-    Vector_Of(char const *) sut = {0};
-
-    Vector_PushBack(&sut, "a");
-    Vector_PushBack(&sut, "quick");
-    Vector_PushBack(&sut, "brown");
-    Vector_PushBack(&sut, "fox");
-
-    Testing_Assert(
-            Vector_Any(sut, value, ({
-                0 == strcmp("brown", value);
-            })),
-            "expected Any to return true"
-    );
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Slice_returns_an_empty_slice_if_start_and_end_are_equal) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-    Vector_PushBack(&sut, 4);
-
-    typedef VectorSlice_Of(int) IntSlice;
-    IntSlice slice = Vector_Slice(IntSlice, sut, 2, 2);
-
-    Testing_Assert(Vector_Empty(slice), "expected slice to be empty");
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Slice_handles_out_of_bounds_start) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-    Vector_PushBack(&sut, 4);
-
-    typedef VectorSlice_Of(int) IntSlice;
-
-    Testing_Assert(Vector_Empty(Vector_SliceFrom(IntSlice, sut, sut.Size)), "expected slice to be empty");
-
-    IntSlice s1 = Vector_Slice(IntSlice, sut, -42, 2);
-    size_t const expectedSize1 = 2;
-    Testing_Assert(expectedSize1 == s1.Size, "expected size to be %zu but was %zu", expectedSize1, s1.Size);
-
-    IntSlice s2 = Vector_Slice(IntSlice, sut, 42, 2);
-    size_t const expectedSize2 = 0;
-    Testing_Assert(expectedSize2 == s2.Size, "expected size to be %zu but was %zu", expectedSize2, s2.Size);
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Slice_handles_out_of_bounds_end) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-    Vector_PushBack(&sut, 4);
-
-    typedef VectorSlice_Of(int) IntSlice;
-
-    Testing_Assert(Vector_Empty(Vector_SliceTo(IntSlice, sut, 0)), "expected slice to be empty");
-
-    IntSlice s1 = Vector_Slice(IntSlice, sut, 2, 42);
-    size_t const expectedSize1 = 2;
-    Testing_Assert(expectedSize1 == s1.Size, "expected size to be %zu but was %zu", expectedSize1, s1.Size);
-
-    IntSlice s2 = Vector_Slice(IntSlice, sut, 2, -42);
-    size_t const expectedSize2 = 0;
-    Testing_Assert(expectedSize2 == s2.Size, "expected size to be %zu but was %zu", expectedSize2, s2.Size);
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Slice_returns_correct_elements) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-    Vector_PushBack(&sut, 4);
-
-    typedef VectorSlice_Of(int) IntSlice;
-
-    size_t start = 1;
-    size_t end = 3;
-    size_t expectedSize = end - start;
-    IntSlice slice = Vector_Slice(IntSlice, sut, start, end);
-
-    Testing_Assert(expectedSize == slice.Size, "expected slice size to be %zu but was %zu", expectedSize, slice.Size);
-    Testing_Assert(
-            Vector_At(sut, start) == slice.Items,
-            "expected slice.Items to point to vector element at %zu", start
-    );
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Slice_returns_all_elememnts_if_start_is_0_and_end_is_Size) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-    Vector_PushBack(&sut, 4);
-
-    typedef VectorSlice_Of(int) IntSlice;
-    IntSlice slice = Vector_Slice(IntSlice, sut, 0, sut.Size);
-
-    Testing_Assert(sut.Size == slice.Size, "expected slice size to be equal to vector size");
-    Testing_Assert(sut.Items == slice.Items, "expected slice.Items to point to vector's Items");
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(Can_take_slice_of_a_slice) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-    Vector_PushBack(&sut, 4);
-    Vector_PushBack(&sut, 5);
-    Vector_PushBack(&sut, 6);
-
-    typedef VectorSlice_Of(int) IntSlice;
-    size_t start1 = 1, end1 = 4;
-    IntSlice s1 = Vector_Slice(IntSlice, sut, start1, end1);
-    size_t start2 = 1, end2 = 2;
-    size_t expectedSize2 = end2 - start2;
-    IntSlice s2 = Vector_Slice(IntSlice, s1, start2, end2);
-
-    Testing_Assert(expectedSize2 == s2.Size, "expected size to be %zu but was %zu", expectedSize2, s2.Size);
-    Testing_Assert(Vector_At(sut, start1 + start2) == s2.Items, "slice's Items points to wrong element");
-
-    Vector_Free(&sut);
-}
-
-Testing_Fact(SliceTo_and_SliceFrom_combined_contain_all_elements_of_initial_vector) {
-    Vector_Of(int) sut = {0};
-
-    Vector_PushBack(&sut, 1);
-    Vector_PushBack(&sut, 2);
-    Vector_PushBack(&sut, 3);
-    Vector_PushBack(&sut, 4);
-    Vector_PushBack(&sut, 5);
-    Vector_PushBack(&sut, 6);
-
-    typedef VectorSlice_Of(int) IntSlice;
-
-    size_t splitIndex = 3;
-    IntSlice s1 = Vector_SliceTo(IntSlice, sut, splitIndex);
-    IntSlice s2 = Vector_SliceFrom(IntSlice, sut, splitIndex);
-
-    Testing_Assert(sut.Size == s1.Size + s2.Size, "expected total size of slices to be equal to vector's Size");
-    Testing_Assert(
-            s1.Items + s1.Size == s2.Items,
-            "expected Items of second slice to begin after items of first slice"
-    );
+    Testing_Assert(Vector_IsEmpty(sut), "expected Empty to return true");
 
     Vector_Free(&sut);
 }
 
 Testing_AllTests = {
-        Testing_AddTest(empty_vector_has_size_and_capacity_of_0),
+        Testing_AddTest(Empty_returns_vector_with_Size_and_Capacity_set_to_0),
+        Testing_AddTest(FromPtr_returns_vector_with_elements_from_given_address),
+        Testing_AddTest(FromArray_returns_vector_with_elements_from_given_automatic_array),
+        Testing_AddTest(FromArray_returns_vector_with_elements_from_given_array_literal),
+        Testing_AddTest(From_returns_vector_with_elements_from_given_struct),
+        Testing_AddTest(Of_returns_vector_with_given_elements),
         Testing_AddTest(PushBack_appends_elements),
         Testing_AddTest(Free_sets_size_and_capacity_to_0),
         Testing_AddTest(TryPopBack_returns_false_for_empty_vector),
         Testing_AddTest(TryPopBack_returns_elements_in_reverse_order),
-        Testing_AddTest(ForEach_never_executes_body_for_empty_list),
-        Testing_AddTest(ForEach_iterates_over_all_elements),
-        Testing_AddTest(At_returns_pointer_to_element_for_valid_non_negative_index),
-        Testing_AddTest(At_returns_pointer_to_element_for_valid_negative_index),
-        Testing_AddTest(At_returns_NULL_for_invalid_indices),
         Testing_AddTest(Empty_returns_true_for_empty_vector),
         Testing_AddTest(Empty_returns_false_for_non_empty),
         Testing_AddTest(Empty_returns_true_for_vector_with_all_elements_removed),
         Testing_AddTest(Empty_returns_true_after_Clear),
-        Testing_AddTest(Any_returns_false_if_no_elements_match),
-        Testing_AddTest(Any_returns_true_if_an_elements_matches),
-        Testing_AddTest(Slice_returns_an_empty_slice_if_start_and_end_are_equal),
-        Testing_AddTest(Slice_handles_out_of_bounds_start),
-        Testing_AddTest(Slice_handles_out_of_bounds_end),
-        Testing_AddTest(Slice_returns_correct_elements),
-        Testing_AddTest(Slice_returns_all_elememnts_if_start_is_0_and_end_is_Size),
-        Testing_AddTest(Can_take_slice_of_a_slice),
-        Testing_AddTest(SliceTo_and_SliceFrom_combined_contain_all_elements_of_initial_vector),
 };
 
 Testing_RunAllTests();
