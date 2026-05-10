43ded2668654dfa8792bd37a0fed6efc32d266e1
reworu <nakayama.reworu@gmail.com>
2026-05-11 01:19:27 +0700

    Add Rpn prefix to types and functions

--- vector/vector_rpn_demo.c
+++ vector/vector_rpn_demo.c
@@ -6,37 +6,37 @@
 
 
 typedef enum {
-    CONST,
-    BIN_OP,
-    UN_OP
-} ItemType;
+    RPN_CONST,
+    RPN_BIN_OP,
+    RPN_UN_OP
+} RpnItemType;
 
 
 typedef struct {
-    ItemType Type;
+    RpnItemType Type;
     union {
         char Op;
         int Value;
     };
-} Item;
+} RpnItem;
 
 
-Item ItemConst(int value) {
-    return (Item) {.Type = CONST, .Value = value};
+RpnItem RpnItemConst(int value) {
+    return (RpnItem) {.Type = RPN_CONST, .Value = value};
 }
 
 
-Item ItemUnOp(char op) {
-    return (Item) {.Type = UN_OP, .Op = op};
+RpnItem RpnItemUnOp(char op) {
+    return (RpnItem) {.Type = RPN_UN_OP, .Op = op};
 }
 
 
-Item ItemBinOp(char op) {
-    return (Item) {.Type = BIN_OP, .Op = op};
+RpnItem RpnItemBinOp(char op) {
+    return (RpnItem) {.Type = RPN_BIN_OP, .Op = op};
 }
 
 
-int EvalBinOp(char op, int arg1, int arg2) {
+int RpnEvalBinOp(char op, int arg1, int arg2) {
     switch (op) {
         case '+':
             return arg1 + arg2;
@@ -54,7 +54,7 @@ int EvalBinOp(char op, int arg1, int arg2) {
 }
 
 
-int EvalUnOp(char op, int arg1) {
+int RpnEvalUnOp(char op, int arg1) {
     if ('-' == op) {
         return -arg1;
     }
@@ -64,7 +64,7 @@ int EvalUnOp(char op, int arg1) {
 }
 
 
-int RpnEval(Item *expression) {
+int RpnEval(RpnItem *expression) {
     if (Array_IsEmpty(expression)) {
         return 0;
     }
@@ -72,21 +72,21 @@ int RpnEval(Item *expression) {
     int *operands = Array_EmptyOfType(int);
     for (typeof(expression) cur = expression; cur != Array_End(expression); cur++) {
         switch (cur->Type) {
-            case CONST:
+            case RPN_CONST:
                 Array_Append(operands, cur->Value);
                 break;
-            case BIN_OP: {
+            case RPN_BIN_OP: {
                 int op2 = Array_Pop(operands);
                 int op1 = Array_Pop(operands);
 
-                Array_Append(operands, EvalBinOp(cur->Op, op1, op2));
+                Array_Append(operands, RpnEvalBinOp(cur->Op, op1, op2));
 
                 break;
             }
-            case UN_OP: {
+            case RPN_UN_OP: {
                 int op1 = Array_Pop(operands);
 
-                Array_Append(operands, EvalUnOp(cur->Op, op1));
+                Array_Append(operands, RpnEvalUnOp(cur->Op, op1));
 
                 break;
             }
@@ -102,7 +102,7 @@ int RpnEval(Item *expression) {
 }
 
 
-void ItemArrayPrint(Item *items, char *end) {
+void ItemArrayPrint(RpnItem *items, char *end) {
     if (NULL == end) {
         end = "\n";
     }
@@ -110,11 +110,11 @@ void ItemArrayPrint(Item *items, char *end) {
     printf("%s=[", nameof_identifier(items));
     for (typeof(items) item = items; item != Array_End(items); item++) {
         switch (item->Type) {
-            case CONST:
+            case RPN_CONST:
                 printf("%d%s", item->Value, item + 1 == Array_End(items) ? "" : " ");
                 break;
-            case BIN_OP:
-            case UN_OP:
+            case RPN_BIN_OP:
+            case RPN_UN_OP:
                 printf("%c%s", item->Op, item + 1 == Array_End(items) ? "" : " ");
                 break;
         }
@@ -127,7 +127,7 @@ void ItemArrayPrint(Item *items, char *end) {
 
 int main() {
     // Create an empty array
-    Item *expression = Array_EmptyOfType(Item);
+    RpnItem *expression = Array_EmptyOfType(RpnItem);
     Array_ForEach(_, expression) {
         LOG_ERROR("This must never be reached");
     }
@@ -135,15 +135,15 @@ int main() {
     ItemArrayPrint(expression, "\n");
 
     // Append single value -> [1]
-    Array_Append(expression, ItemConst(1));
+    Array_Append(expression, RpnItemConst(1));
     ItemArrayPrint(expression, "\n");
 
     // Extend with several values -> [1 2 +]
-    Array_ExtendWith(expression, ItemConst(2), ItemBinOp('+'));
+    Array_ExtendWith(expression, RpnItemConst(2), RpnItemBinOp('+'));
     ItemArrayPrint(expression, "\n");
 
     // Temporary array with more items
-    Item *minus_two = Array_Of(ItemConst(2), ItemUnOp('-'));
+    RpnItem *minus_two = Array_Of(RpnItemConst(2), RpnItemUnOp('-'));
 
     // Extend the main array with another array -> [1 2 + 2 -]
     expression = Array_Extend(expression, minus_two);
@@ -153,16 +153,16 @@ int main() {
     // Extend with array literal -> [1 2 + 2 - *]
     Array_ExtendWithValues(
             expression,
-            ((Item[]) {ItemBinOp('*')}),
+            ((RpnItem[]) {RpnItemBinOp('*')}),
             1,
-            sizeof(Item)
+            sizeof(RpnItem)
     );
     ItemArrayPrint(expression, "\n");
 
     // Temporary array with more items
-    Item *plus_one = Array_OfType(Item, 2);
-    plus_one[0] = ItemConst(1);
-    plus_one[1] = ItemBinOp('+');
+    RpnItem *plus_one = Array_OfType(RpnItem, 2);
+    plus_one[0] = RpnItemConst(1);
+    plus_one[1] = RpnItemBinOp('+');
 
     // Extend the main array with another array -> [1 2 + 2 - * 1 +]
     expression = Array_Extend(expression, plus_one);
