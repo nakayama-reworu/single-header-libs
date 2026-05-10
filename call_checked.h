6cc860ca5610c0c1d9d2c559bfb9c8a8a3bfd339
reworu <nakayama.reworu@gmail.com>
2026-05-11 01:19:33 +0700

    Make CallChecked print arguments list on error

--- call_checked.h
+++ call_checked.h
@@ -5,17 +5,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CallChecked(Callee, ArgsList)   \
-({                                      \
-    errno = 0;                          \
-    __auto_type _r = Callee ArgsList;   \
-    if (errno) {                        \
-        fprintf(                        \
-            stderr, "[%s:%d] %s: %s\n", \
-            __FILE_NAME__, __LINE__,    \
-            #Callee, strerror(errno)    \
-        );                              \
-        exit(EXIT_FAILURE);             \
-    }                                   \
-    _r;                                 \
+#define CallChecked(Callee, ArgsList)       \
+({                                          \
+    errno = 0;                              \
+    __auto_type _r = Callee ArgsList;       \
+    if (errno) {                            \
+        fprintf(                            \
+            stderr, "[%s:%d] %s%s: %s\n",   \
+            __FILE_NAME__, __LINE__,        \
+            #Callee, #ArgsList,             \
+            strerror(errno)                 \
+        );                                  \
+        exit(EXIT_FAILURE);                 \
+    }                                       \
+    _r;                                     \
 })
